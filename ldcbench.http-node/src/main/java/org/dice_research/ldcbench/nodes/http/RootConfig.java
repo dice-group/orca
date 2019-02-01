bfc0f96d38d7ad2f495f138e1ff12df500b2ce81
Micha <michael.roeder@uni-paderborn.de>
2019-02-01 19:57:06 +0100

    Started to implement the HTTP dereferencing node.

--- ldcbench.http-node/src/main/java/org/dice_research/ldcbench/nodes/http/RootConfig.java
+++ ldcbench.http-node/src/main/java/org/dice_research/ldcbench/nodes/http/RootConfig.java
@@ -1,5 +1,20 @@
 package org.dice_research.ldcbench.nodes.http;
 
-public class RootConfig {
+import java.util.List;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "org.dice_research.ldcbench.nodes.http")
+public class RootConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(new TripleStreamConverter());
+    }
 }
