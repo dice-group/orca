3749702f41769fb39f1aaa618611f6cb21785567
Micha <michael.roeder@uni-paderborn.de>
2019-02-14 12:39:11 +0100

    Moved the spring based classes into an own package.

--- ldcbench.http-node/src/main/java/org/dice_research/ldcbench/nodes/http/spring/RobotsController.java
+++ ldcbench.http-node/src/main/java/org/dice_research/ldcbench/nodes/http/spring/RobotsController.java
@@ -1,4 +1,4 @@
-package org.dice_research.ldcbench.nodes.http;
+package org.dice_research.ldcbench.nodes.http.spring;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
