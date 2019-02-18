f0d8e6879c420e31345b04de5e86d6a109832722
Denis Kuchelev <denkv@users.noreply.github.com>
2019-02-18 19:13:05 +0100

    Apply editorconfig

--- ldcbench.api/src/main/java/org/dice_research/ldcbench/ApiConstants.java
+++ ldcbench.api/src/main/java/org/dice_research/ldcbench/ApiConstants.java
@@ -1,11 +1,11 @@
 package org.dice_research.ldcbench;
 
 public class ApiConstants {
-    
+
     public static final String ENV_NODE_ID_KEY = "LDCBENCH_NODE_ID";
     public static final String ENV_BENCHMARK_EXCHANGE_KEY = "LDCBENCH_BENCHMARK_EXCHANGE";
     public static final String ENV_DATA_QUEUE_KEY = "LDCBENCH_DATA_QUEUE";
-    
+
     public static final String ENV_HTTP_PORT_KEY = "HTTP_PORT";
 
     public static final byte NODE_READY_SIGNAL = 124;
