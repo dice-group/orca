a250eac9d8f9f24b779dddc5fe862c3f1106370d
Micha <michael.roeder@uni-paderborn.de>
2019-02-18 17:04:16 +0100

    Added the triple creation to the API module.

---
+++ ldcbench.api/src/main/java/org/dice_research/ldcbench/rdf/TripleCreator.java
@@ -0,0 +1,15 @@
+package org.dice_research.ldcbench.rdf;
+
+import org.apache.jena.graph.Triple;
+
+/**
+ * An interface for a class which can generate a triple based on Ids that it got
+ * from a graph.
+ * 
+ * @author Michael R&ouml;der (michael.roeder@uni-paderborn.de)
+ *
+ */
+public interface TripleCreator {
+
+    public Triple createTriple(int sourceId, int propertyId, int targetId, int targetExtId, int targetExtGraphId);
+}
