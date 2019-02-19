ac506ecc432839042cac20ec8be941bc8590612e
Micha <michael.roeder@uni-paderborn.de>
2019-02-19 15:23:01 +0100

    Added the implementation for the file based graph supplier.

--- ldcbench.controller/src/main/java/org/dice_research/ldcbench/benchmark/eval/FileBasedGraphSupplier.java
+++ ldcbench.controller/src/main/java/org/dice_research/ldcbench/benchmark/eval/FileBasedGraphSupplier.java
@@ -1,11 +1,24 @@
 package org.dice_research.ldcbench.benchmark.eval;
 
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
 import org.dice_research.ldcbench.graph.Graph;
+import org.dice_research.ldcbench.graph.serialization.SerializationHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class FileBasedGraphSupplier implements GraphSupplier {
-    
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(FileBasedGraphSupplier.class);
+
     protected String[] graphFiles;
     protected String[] domains;
+
+    public FileBasedGraphSupplier(String[] graphFiles, String[] domains) {
+        this.graphFiles = graphFiles;
+        this.domains = domains;
+    }
 
     @Override
     public int getNumberOfGraphs() {
@@ -14,7 +27,11 @@ public class FileBasedGraphSupplier implements GraphSupplier {
 
     @Override
     public Graph getGraph(int id) {
-        // TODO
+        try {
+            return SerializationHelper.deserialize(FileUtils.readFileToByteArray(new File(graphFiles[id])));
+        } catch (Exception e) {
+            LOGGER.error("Couldn't load graph #" + id + ". Returning null.", e);
+        }
         return null;
     }
 
@@ -22,5 +39,5 @@ public class FileBasedGraphSupplier implements GraphSupplier {
     public String[] getDomains() {
         return domains;
     }
-    
+
 }
