304ac9ef6538fd4fc7eef39350f6e2435360b58f
Abdelmoneim Desouki <31845116+abdoudesouki@users.noreply.github.com>
2019-03-27 13:29:42 +0100

    notes

--- ldcbench.api/src/main/java/org/dice_research/ldcbench/generate/RandomCloudGraph.java
+++ ldcbench.api/src/main/java/org/dice_research/ldcbench/generate/RandomCloudGraph.java
@@ -9,10 +9,10 @@ public class RandomCloudGraph implements GraphGenerator{
 	@Override
 public void generateGraph(int numberOfNodes, double avgDegree, long seed, GraphBuilder builder) {
 /*
-		generate bi-partite (up to 4 parts(number of types)):1000 hubs 500 endpoints etc.
-		endpoints connected to each other with power law
+		generate bi-partite (up to 4 parts(number of types)):1000 hubs, 500 endpoints etc.
+		endpoints(same part): connected to each other with power law
 		-type0,type1,type2,type3,type4:
-		-parts p0,p1,p2,p3 upto p4: each part contains 1 or more types that can connect
+		-parts p0,p1,p2,p3 up to p4: each part contains 1 or more types that can connect
 		-assign parts to types (Hard code type0 as hubs): start with hubs (sort and FIFS) assign remaining
 		-What is d? d is one parameter to the algorithm.
 		-Call graphGenerator for each part with number of nodes in that part and d.
