6f209bf975b86b31f77bfd82b084fa7710fe60c0
Denis Kuchelev <denkv@users.noreply.github.com>
2019-04-02 14:36:25 +0200

    Apply .editorconfig

--- ldcbench.ckan-node/src/main/java/org/dice_research/ldcbench/nodes/ckan/Constants.java
+++ ldcbench.ckan-node/src/main/java/org/dice_research/ldcbench/nodes/ckan/Constants.java
@@ -1,27 +1,23 @@
 package org.dice_research.ldcbench.nodes.ckan;
 
 public class Constants {
-	
+
 	public static final String TOKEN_API = "8b40fc3b-2889-4341-a3bd-c8ff16661544";
 	public static final String ORGANIZATION = "diceupb";
-	
+
 	//images used
-	
+
 	public static final String CKAN = "mwauer/ckan:2.8.1-opal";
 	public static final String POSTGRES = "postgres:9.6.0";
 	public static final String SOLR = "earthquakesan/ckan-solr:2.8.0";
 	public static final String REDIS = "redis:4.0.11";
-	
-	
+
+
 	// keys required by ckan
 
 	public static final String CKAN_RECAPTCHA_PUBLICKEY = "";
 	public static final String CKAN_RECAPTCHA_PRIVATEKEY = "";
-	
+
 	public static final String ADMIN_TOKEN = "8b40fc3b-2889-4341-a3bd-c8ff16661544";
-	
-
-
-
 
 }
