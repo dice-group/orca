d172d8d83df688a1fed18b31669e23ec1c6d9ec6
Denis Kuchelev <denkv@users.noreply.github.com>
2019-07-10 10:29:55 +0200

    Add a test when nodes are of the same type

--- ldcbench.api/src/test/java/org/dice_research/ldcbench/generate/SmallRandomCloudTest.java
+++ ldcbench.api/src/test/java/org/dice_research/ldcbench/generate/SmallRandomCloudTest.java
@@ -22,6 +22,17 @@ public class SmallRandomCloudTest {
 
     @Test
     @Ignore
+    public void testNodesOfSameType() {
+        int[] nodetypes = {0, 0, 0, 0};
+        boolean[] ishub = {false, true, false};
+        int[][] typeconn = {{1,0,0},{1,1,1},{1,0,0}};
+        RandomCloudGraph rg = new RandomCloudGraph("Graph", nodetypes, ishub, typeconn);
+        GrphBasedGraph g = new GrphBasedGraph();
+        rg.generateGraph(4, 2.0, 0, g);
+    }
+
+    @Test
+    @Ignore
     public void testNodeCounts() {
         int[] typecounts = {1, 1, 0}; // 1 node of first type, 1 node of second type, 0 nodes of third type
         int hcount = 0; // What to pass there?
