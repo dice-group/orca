60c822438119374c5eb198f1bb96618c37678a8e
Micha <michael.roeder@uni-paderborn.de>
2019-08-05 17:18:27 +0200

    Added dump node functionality to the simple http server component.

--- ldcbench.http-node/src/main/java/org/dice_research/ldcbench/nodes/http/simple/SimpleHttpServerComponent.java
+++ ldcbench.http-node/src/main/java/org/dice_research/ldcbench/nodes/http/simple/SimpleHttpServerComponent.java
@@ -4,6 +4,8 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
+import java.util.Objects;
+import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 import org.apache.commons.io.IOUtils;
@@ -12,6 +14,7 @@ import org.apache.jena.rdf.model.ModelFactory;
 import org.dice_research.ldcbench.ApiConstants;
 import org.dice_research.ldcbench.graph.Graph;
 import org.dice_research.ldcbench.nodes.components.AbstractNodeComponent;
+import org.dice_research.ldcbench.nodes.http.simple.dump.DumpFileResource;
 import org.dice_research.ldcbench.rdf.UriHelper;
 import org.hobbit.core.components.Component;
 import org.hobbit.utils.EnvVariables;
@@ -32,15 +35,16 @@ public class SimpleHttpServerComponent extends AbstractNodeComponent implements 
     protected Container container;
     protected Server server;
     protected Connection connection;
+    protected boolean dumpFileNode;
 
     @Override
     public void initBeforeDataGeneration() throws Exception {
         // check whether this node contains dump files
-        boolean dumpFileNode = EnvVariables.getBoolean("LDCBENCH_USE_DUMP_FILE",false);        
-        if(dumpFileNode) {
+        dumpFileNode = EnvVariables.getBoolean("LDCBENCH_USE_DUMP_FILE", false);
+        if (dumpFileNode) {
             LOGGER.debug("Init as HTTP dump file node.");
-            this.resourceUriTemplate = "";
-            this.accessUriTemplate = "";
+            this.resourceUriTemplate = "http://" + myself + "/dumpFile.ttl#%s-%s/%s-%s";
+            this.accessUriTemplate = "http://" + myself + "/dumpFile.ttl#%s-%s/%s-%s";
         } else {
             LOGGER.debug("Init as dereferencing HTTP node.");
         }
@@ -60,15 +64,25 @@ public class SimpleHttpServerComponent extends AbstractNodeComponent implements 
     }
 
     protected Container createContainer() {
-        // Create the container based on the information that has been received
-        return new CrawleableResourceContainer(new GraphBasedResource(cloudNodeId,
-                Stream.of(nodeMetadata).map(nm -> nm.getResourceUriTemplate()).toArray(String[]::new),
-                Stream.of(nodeMetadata).map(nm -> nm.getAccessUriTemplate()).toArray(String[]::new),
-                graphs.toArray(new Graph[graphs.size()]), (r -> r.getTarget().contains(UriHelper.DATASET_KEY_WORD)
-                        && r.getTarget().contains(UriHelper.RESOURCE_NODE_TYPE)),
-                new String[] {
-                // "application/rdf+xml", "text/plain", "*/*"
-                }));
+        CrawleableResource resource = null;
+        if (dumpFileNode) {
+            resource = DumpFileResource.create(cloudNodeId,
+                    Stream.of(nodeMetadata).map(nm -> nm.getResourceUriTemplate()).toArray(String[]::new),
+                    Stream.of(nodeMetadata).map(nm -> nm.getAccessUriTemplate()).toArray(String[]::new),
+                    graphs.toArray(new Graph[graphs.size()]), (r -> true), new String[] {});
+        } else {
+            // Create the container based on the information that has been received
+            resource = new GraphBasedResource(cloudNodeId,
+                    Stream.of(nodeMetadata).map(nm -> nm.getResourceUriTemplate()).toArray(String[]::new),
+                    Stream.of(nodeMetadata).map(nm -> nm.getAccessUriTemplate()).toArray(String[]::new),
+                    graphs.toArray(new Graph[graphs.size()]), (r -> r.getTarget().contains(UriHelper.DATASET_KEY_WORD)
+                            && r.getTarget().contains(UriHelper.RESOURCE_NODE_TYPE)),
+                    new String[] {
+                    // "application/rdf+xml", "text/plain", "*/*"
+                    });
+        }
+        Objects.requireNonNull(resource, "Couldn't create crawleable resource. Exiting.");
+        return new CrawleableResourceContainer(resource);
     }
 
     protected Model readModel(String modelFile, String modelLang) {
