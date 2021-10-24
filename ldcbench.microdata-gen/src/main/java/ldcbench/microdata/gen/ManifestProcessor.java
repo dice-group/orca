9d76729b005bf6d4775a92c12714b39df16f2708
Thoren-G <thoren-gruettemeier@web.de>
2021-10-24 14:47:53 +0200

    added Microdata generator

---
+++ ldcbench.microdata-gen/src/main/java/ldcbench/microdata/gen/ManifestProcessor.java
@@ -0,0 +1,73 @@
+package org.dice_research.ldcbench.microdata.gen;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.jena.query.Dataset;
+import org.apache.jena.query.DatasetFactory;
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.query.ResultSet;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Processes the manifest.ttl files of the Microdata test suite and extracts the
+ * pairs of HTML and TTL files of all tests.
+ *
+ * @author Michael Röder (michael.roeder@uni-paderborn.de)
+ * @author Thoren Grüttemeier (thoreng@uni-paderborn.de, Microdata modifications)
+ *
+ */
+public class ManifestProcessor {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ManifestProcessor.class);
+
+    private static final String SELECT_QUERY_FOR_FILES = "SELECT ?f1 ?f2 WHERE { ?t a <http://www.w3.org/ns/rdftest##TestMicrodataEval> . "
+            + "?t <http://www.w3.org/2001/sw/DataAccess/tests/test-manifest#action> ?f1 ."
+            + "?t <http://www.w3.org/2001/sw/DataAccess/tests/test-manifest#result> ?f2 . }"; //TODO: filter the schema tests?
+    private static final String SELECTED_HTML_FILE = "f1";
+    private static final String SELECTED_TTL_FILE = "f2";
+
+    /**
+     * Creates a mapping of html file to ttl file.
+     *
+     * @param manifestFile
+     * @return
+     */
+    public Map<String, String> loadTests(String manifestFile) {
+        try (InputStream is = new BufferedInputStream(new FileInputStream(new File(manifestFile)))) {
+            return loadTests(is);
+        } catch (IOException e) {
+            LOGGER.error("Couldn't load manifest file \"" + manifestFile + "\". Returning null.", e);
+        }
+        return null;
+    }
+
+    public Map<String, String> loadTests(InputStream is) {
+        Model model = ModelFactory.createDefaultModel();
+        model.read(is, "", "TTL");
+        Dataset dataset = DatasetFactory.create(model);
+        Query query = QueryFactory.create(SELECT_QUERY_FOR_FILES);
+        QueryExecution qe = QueryExecutionFactory.create(query, dataset);
+        ResultSet rs = qe.execSelect();
+        Map<String, String> tests = new HashMap<String, String>();
+        QuerySolution qs;
+        while (rs.hasNext()) {
+            qs = rs.next();
+            tests.put(qs.get(SELECTED_HTML_FILE).asResource().getURI(),
+                    qs.get(SELECTED_TTL_FILE).asResource().getURI());
+        }
+        return tests;
+    }
+}
---
+++ ldcbench.microdata-gen/src/main/java/ldcbench/microdata/gen/MicrodataGenerator.java
@@ -0,0 +1,185 @@
+package org.dice_research.ldcbench.microdata.gen;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+
+import org.apache.commons.io.FileUtils;
+import org.dice_research.ldcbench.ApiConstants;
+import org.dice_research.ldcbench.benchmark.DataGenerator;
+import org.dice_research.ldcbench.graph.Graph;
+import org.dice_research.ldcbench.rdf.SimpleTripleCreator;
+import org.dice_research.ldcbench.utils.tar.TarFileGenerator;
+import org.hobbit.core.rabbit.RabbitQueueFactory;
+import org.hobbit.core.rabbit.SimpleFileSender;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MicrodataGenerator extends DataGenerator {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MicrodataGenerator.class);
+
+    public static final String ENTRANCE_HTML_FILE_NAME = "entrance.html";
+    public static final String ENTRANCE_TTL_FILE_NAME = "entrance.ttl";
+    public static final String ACCESS_URI_TEMPLATE_PATTERN = "/%s-%s/%s-%s";
+
+    protected static final File ENTRANCE_HTML_FILE = new File(ENTRANCE_HTML_FILE_NAME);
+    protected static final File ENTRANCE_TTL_FILE = new File(ENTRANCE_TTL_FILE_NAME);
+
+    // "../ldcbench.microdata-gen/" makes it work in ldcbench.controller tests
+    protected static final String TEST_ROOT_DIRECTORY = "../ldcbench.microdata-gen/w3c.github.io_microdata-rdf/tests/";
+
+    protected static final String MICRODATA_TEST_DOMAIN = "http://mcdata.info/test-suite/tests/"; //TODO?
+
+    // unlike to rdfa, we only have one test case
+    // protected static final String TEST_CASES[] = new String[] { "rdfa1.0/html4", "rdfa1.0/xhtml1", "rdfa1.1/html4",
+    //         "rdfa1.1/html5", "rdfa1.1/xhtml1", "rdfa1.1/xhtml5" };
+
+    @Override
+    protected void sendFinalGraph(Graph graph) throws Exception {
+        // // iterate over the available test sets
+        LOGGER.info("Reading manifest files of different test cases...");
+        Map<String, String> tests = loadTestFiles();
+        LOGGER.info("Found {} test files. Processing them...", tests.size());
+        SortedMap<String, File> htmlFiles = new TreeMap<>();
+        SortedMap<String, File> ttlFiles = new TreeMap<>();
+        convertTestUrisToFiles(tests.keySet(), htmlFiles);
+        convertTestUrisToFiles(tests.values(), ttlFiles);
+
+        // Generate URL of resources on the node
+        String nodeDomain = accessUriTemplates[getNodeId()].replace(ACCESS_URI_TEMPLATE_PATTERN,
+                "") + "/";
+        // Load the tests and replace URIs
+        replaceUrisInFiles(htmlFiles, MICRODATA_TEST_DOMAIN, nodeDomain);
+        replaceUrisInFiles(ttlFiles, MICRODATA_TEST_DOMAIN, nodeDomain);
+        htmlFiles = replaceUrisInMapping(htmlFiles, MICRODATA_TEST_DOMAIN, nodeDomain);
+        ttlFiles = replaceUrisInMapping(ttlFiles, MICRODATA_TEST_DOMAIN, nodeDomain);
+
+        // Generate HTML and TTL file based on the graph and the list of test files
+        generateEntranceFile(graph, htmlFiles, ENTRANCE_HTML_FILE, ENTRANCE_TTL_FILE);
+        // Add entrance file to the list of HTML and ttl files
+        htmlFiles.put(ENTRANCE_HTML_FILE_NAME, ENTRANCE_HTML_FILE);
+        ttlFiles.put(ENTRANCE_TTL_FILE_NAME, ENTRANCE_TTL_FILE);
+
+        String filePrefix = String.format("graph-%0" + (int) Math.ceil(Math.log10(getNumberOfGenerators() + 1)) + "d",
+                getNodeId());
+
+        // Generate HTML tar file
+        generateAndSendTarFile(filePrefix + ApiConstants.FILE_ENDING_HTML_TAR_GZ, htmlFiles, outgoingDataQueuefactory, dataQueueName);
+
+        // Generate TTL tar file
+        generateAndSendTarFile(filePrefix + ApiConstants.FILE_ENDING_TTL_TAR_GZ, ttlFiles, outgoingDataQueuefactory,
+                evalDataQueueName);
+    }
+
+    protected void generateEntranceFile(Graph graph, Map<String, File> htmlFiles, File entranceFile, File entranceTTLFile)
+            throws IOException {
+        SimpleTripleCreator creator = new SimpleTripleCreator(getNodeId(), resourceUriTemplates, accessUriTemplates);
+
+        Set<String> outgoingLinks = generateOutgoingLinks(graph, creator);
+        outgoingLinks.addAll(htmlFiles.keySet());
+
+        String entranceUri = generateEntranceNodeUri(graph, creator);
+
+        MicrodataEntranceFileGenerator generator = new MicrodataEntranceFileGenerator();
+        generator.generate(entranceFile, entranceTTLFile, entranceUri, outgoingLinks);
+    }
+
+    protected String generateEntranceNodeUri(Graph graph, SimpleTripleCreator creator) {
+        int entranceNode = graph.getEntranceNodes()[0];
+        return creator
+                .createNode(entranceNode, graph.getExternalNodeId(entranceNode), graph.getGraphId(entranceNode), false)
+                .getURI();
+    }
+
+    protected Set<String> generateOutgoingLinks(final Graph graph, final SimpleTripleCreator creator) {
+        Set<String> outgoingLinks = new HashSet<>();
+        int entranceNodes[] = graph.getEntranceNodes();
+        for (int i = 0; i < entranceNodes.length; ++i) {
+            outgoingLinks.addAll(Arrays.stream(graph.outgoingEdgeTargets(entranceNodes[i]))
+                    .mapToObj(id -> creator.createNode(id, graph.getExternalNodeId(id), graph.getGraphId(id), false))
+                    .map(n -> n.getURI()).collect(Collectors.toSet()));
+        }
+        return outgoingLinks;
+    }
+
+    protected static void convertTestUrisToFiles(Collection<String> testUris, Map<String, File> files) {
+        for (String uri : testUris) {
+            files.put(uri, new File(TEST_ROOT_DIRECTORY, uri));
+        }
+    }
+
+    protected static SortedMap<String, File> replaceUrisInMapping(Map<String, File> uriFileMapping, String regex,
+            String replacement) {
+        final SortedMap<String, File> newMap = new TreeMap<>();
+        uriFileMapping.entrySet().stream()
+                .forEach(e -> newMap.put(e.getKey().replaceAll(regex, replacement), e.getValue()));
+        return newMap;
+    }
+
+    protected static void replaceUrisInFiles(Map<String, File> files, String regex, String replacement) {
+        String fileContent;
+        File origFile = null;
+        File newFile;
+        for (Entry<String, File> entry : files.entrySet()) {
+            try {
+                origFile = entry.getValue();
+                newFile = File.createTempFile("new", origFile.getName());
+                newFile.deleteOnExit();
+                fileContent = FileUtils.readFileToString(origFile, StandardCharsets.UTF_8);
+                fileContent = fileContent.replaceAll(regex, replacement);
+                FileUtils.write(newFile, fileContent, StandardCharsets.UTF_8);
+                entry.setValue(newFile);
+            } catch (IOException e) {
+                LOGGER.error("Couldn't update file content of " + origFile.getAbsolutePath() + ". It will be ignored.", e);
+            }
+        }
+    }
+
+    protected static Map<String, String> loadTestFiles() {
+        ManifestProcessor processor = new ManifestProcessor();
+        Map<String, String> testFiles = new HashMap<>();
+
+        StringBuilder pathBuilder = new StringBuilder();
+        pathBuilder.append(TEST_ROOT_DIRECTORY);
+        // pathBuilder.append(TEST_CASES[i]); // unlike to rdfa we only have one testcase
+        pathBuilder.append("/manifest.ttl");
+        testFiles.putAll(processor.loadTests(pathBuilder.toString()));
+
+        return testFiles;
+    }
+
+    protected void generateAndSendTarFile(String file, SortedMap<String, File> files,
+            RabbitQueueFactory outgoingDataQueuefactory, String dataQueueName) throws IOException {
+        File tarFile = new File(file);
+        // Create file
+        TarFileGenerator generator = new TarFileGenerator();
+        generator.generateTarFile(tarFile, files, true);
+
+        // Send file
+        sendTarFile(tarFile, outgoingDataQueuefactory, dataQueueName);
+    }
+
+    protected static void sendTarFile(File file, RabbitQueueFactory outgoingDataQueuefactory, String dataQueueName)
+            throws IOException {
+        // Create file
+        try (InputStream is = new BufferedInputStream(new FileInputStream(file));
+                SimpleFileSender dataSender = SimpleFileSender.create(outgoingDataQueuefactory, dataQueueName);) {
+            dataSender.streamData(is, file.getName());
+        }
+    }
+
+}
